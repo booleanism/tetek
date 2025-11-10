5b5a2c57c6085b5784be1439989c161467f3931d
Mambaul Hisam <hisyamsquartz@gmail.com>
2025-11-10 21:30:26 +0700

    feat(auth): utilizing generated code

--- feeds/cmd/http/middleware/auth.go
+++ feeds/cmd/http/middleware/auth.go
@@ -1,19 +1,17 @@
 package middleware
 
 import (
-	"strings"
+	"context"
+	"regexp"
+	"time"
 
 	"github.com/booleanism/tetek/auth/amqp"
+	"github.com/booleanism/tetek/comments/cmd/http/api"
 	"github.com/booleanism/tetek/feeds/internal/contract"
 	"github.com/booleanism/tetek/pkg/errro"
 	"github.com/booleanism/tetek/pkg/helper"
 	"github.com/gofiber/fiber/v3"
-	"github.com/google/uuid"
 )
-
-type authRequest struct {
-	Authorization string `header:"Authorization"`
-}
 
 func OptionalAuth(auth *contract.LocalAuthContr) fiber.Handler {
 	return func(ctx fiber.Ctx) error {
@@ -22,21 +20,37 @@ func OptionalAuth(auth *contract.LocalAuthContr) fiber.Handler {
 			return ctx.Next()
 		}
 
-		if err := actualAuth(ctx, auth, jwt); err != nil {
+		// The consequencies about this implementation that is
+		// system assumes it's valid jwt format if user passes Authorization header.
+		// And if auth worker dead, then it will wait for 5 second to make sure.
+		// Keep calm, it's affected only and only if auth worker is dead.
+		id := ctx.Locals(helper.RequestIdKey{}).(string)
+		cto, cancel := context.WithTimeout(
+			context.WithValue(
+				ctx, helper.RequestIdKey{},
+				id,
+			),
+			5*time.Second,
+		)
+		defer cancel()
+
+		var authRes *amqp.AuthResult
+		if err := actualAuth(cto, auth, jwt, &authRes); err != nil {
 			return ctx.Next()
 		}
 
+		if authRes == nil {
+			ctx.Next()
+		}
+
+		ctx.Locals("jwt", authRes)
 		return ctx.Next()
 	}
 }
 
 func checkJwt(ctx fiber.Ctx) (string, errro.Error) {
-	req := authRequest{}
-	if err := helper.BindRequest(ctx, &req); err != nil {
-		return "", errro.New(errro.INVALID_REQ, err.Error())
-	}
-
-	if req.Authorization == "" {
+	jwt, ok := ctx.Locals(api.BearerAuthScopes).(string)
+	if !ok {
 		res := helper.GenericResponse{
 			Code:    errro.EAUTH_MISSING_HEADER,
 			Message: "missing authorization header",
@@ -45,11 +59,11 @@ func checkJwt(ctx fiber.Ctx) (string, errro.Error) {
 		return "", e.WithDetail(res.Json(), errro.TDETAIL_JSON)
 	}
 
-	jwt, ok := strings.CutPrefix(req.Authorization, "Bearer ")
-	if !ok {
+	r := regexp.MustCompile(`^(?:[\w-]*\.){2}[\w-]*$`)
+	if !r.Match([]byte(jwt)) {
 		res := helper.GenericResponse{
-			Code:    errro.EAUTH_MISSMATCH_AUTH_MECHANISM,
-			Message: "mismatch authorization mechanism",
+			Code:    errro.EAUTH_JWT_MALFORMAT,
+			Message: "malformat jwt",
 		}
 		e := errro.New(res.Code, res.Message)
 		return "", e.WithDetail(res.Json(), errro.TDETAIL_JSON)
@@ -69,18 +83,43 @@ func Auth(auth *contract.LocalAuthContr) fiber.Handler {
 			return er.WithDetail(res.Json(), errro.TDETAIL_JSON).SendError(ctx, fiber.StatusBadRequest)
 		}
 
-		if err := actualAuth(ctx, auth, jwt); err != nil {
+		id := ctx.Locals(helper.RequestIdKey{}).(string)
+		cto, cancel := context.WithTimeout(
+			context.WithValue(
+				ctx, helper.RequestIdKey{},
+				id,
+			),
+			10*time.Second,
+		)
+		defer cancel()
+
+		var authRes *amqp.AuthResult
+		if err := actualAuth(cto, auth, jwt, &authRes); err != nil {
+			if err.Code() == errro.EAUTH_SERVICE_UNAVAILABLE {
+				return err.SendError(ctx, fiber.StatusRequestTimeout)
+			}
+
 			return err.SendError(ctx, fiber.StatusUnauthorized)
 		}
+		if authRes == nil {
+			res := helper.GenericResponse{
+				Code:    errro.EAUTH_JWT_VERIFY_FAIL,
+				Message: "authorization failed",
+			}
+
+			e := errro.New(res.Code, res.Message)
+			return e.WithDetail(res.Json(), errro.TDETAIL_JSON).SendError(ctx, fiber.StatusUnauthorized)
+		}
+
+		ctx.Locals("jwt", authRes)
 
 		return ctx.Next()
 	}
 }
 
-func actualAuth(ctx fiber.Ctx, auth *contract.LocalAuthContr, jwt string) errro.ResError {
-	id := uuid.NewString()
+func actualAuth(ctx context.Context, auth *contract.LocalAuthContr, jwt string, authRes **amqp.AuthResult) errro.ResError {
 	task := amqp.AuthTask{Jwt: jwt}
-	if err := auth.Publish(id, task); err != nil {
+	if err := auth.Publish(ctx, task); err != nil {
 		res := helper.GenericResponse{
 			Code:    errro.EAUTH_SERVICE_UNAVAILABLE,
 			Message: "auth service unavailable: publishing auth task",
@@ -89,7 +128,7 @@ func actualAuth(ctx fiber.Ctx, auth *contract.LocalAuthContr, jwt string) errro.
 		return e.WithDetail(res.Json(), errro.TDETAIL_JSON)
 	}
 
-	authRes, err := auth.Consume(id)
+	authr, err := auth.Consume(ctx)
 	if err != nil {
 		res := helper.GenericResponse{
 			Code:    errro.EAUTH_SERVICE_UNAVAILABLE,
@@ -99,8 +138,8 @@ func actualAuth(ctx fiber.Ctx, auth *contract.LocalAuthContr, jwt string) errro.
 		return e.WithDetail(res.Json(), errro.TDETAIL_JSON)
 	}
 
-	if authRes.Code == errro.SUCCESS {
-		ctx.Locals("jwt", authRes)
+	if authr.Code == errro.SUCCESS {
+		authRes = &authr
 		return nil
 	}
 
